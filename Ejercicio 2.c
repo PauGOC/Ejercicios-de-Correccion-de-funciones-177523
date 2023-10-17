cb4af05206811e4e389bf636126860a3d5c9d4e4
PauGOC <141955969+PauGOC@users.noreply.github.com>
2023-10-17 12:13:30 -0600

    Add files via upload

---
+++ Ejercicio 2.c
@@ -0,0 +1,33 @@
+//Ejercicio 2: Corregir más errores -177523
+#include <stdio.h>
+
+float perimetroFigura(float numero);
+float areaFigura(float numero);
+
+float perimetroFigura(float numero) {
+    float perimetro;
+    perimetro = 4 * numero;
+    return perimetro;
+}
+
+float areaFigura(float numero) {
+    float area;
+    area = numero * numero;
+    return area;
+}
+
+int main() {
+    float num;
+    printf("Lado de un cuadrado: ");
+    scanf("%f", &num);
+
+    printf("Perimetro: %f\n", perimetroFigura(num));
+    printf("Area: %f\n", areaFigura(num));
+
+    return 0;
+}
+
+
+
+
+
